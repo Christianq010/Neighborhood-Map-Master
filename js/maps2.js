1ee4e26168d5ac40693479f093c9af472230ac37
Christiaan Quyn <christianq010@gmai.com>
2017-02-12 14:21:14 +0530

    fix: Zoom button not working -
    Hide, show listings and Auto-Correct Working not zoom into location does not work

--- js/maps2.js
+++ js/maps2.js
@@ -155,7 +155,7 @@ function initMap() {
 
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7413549, lng: -73.9980244},
+        center: {lat: 6.911652, lng: 79.849640},
         zoom: 13,
         styles: styles,
         mapTypeControl: false
@@ -173,12 +173,9 @@ function initMap() {
     // These are the real estate listings that will be shown to the user.
     // Normally we'd have these in a database instead.
     var locations = [
-        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+        {title: 'Carnival', location: {lat: 6.910468, lng: 79.850137}},
+        {title: 'Baskin Robbins', location: {lat: 6.908018, lng: 79.850759}},
+        {title: 'Roots - Crescat', location: {lat: 6.918126, lng: 79.848195}}
     ];
 
     var largeInfowindow = new google.maps.InfoWindow();
