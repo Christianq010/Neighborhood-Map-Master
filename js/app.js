af3bc4a4f3d3b9d48d13cb0bdcaa7c8f34ff582a
Christiaan Quyn <christianq010@gmail.com>
2017-02-21 17:16:28 +0530

    refactor: complete refactoring of code in main Class and locations in ViewModel

--- js/app.js
+++ js/app.js
@@ -1,296 +1,141 @@
 
-var Model = {
-    locations: [
-        {
-            title: 'Carnival',
-            lat: 6.910468,
-            lng: 79.850137
-        },
-        {
-            title: 'Baskin Robbins',
-            lat: 6.908018,
-            lng: 79.850759
-        },
-        {
-            title: 'Roots - Crescat',
-            lat: 6.918126,
-            lng: 79.848195
-        },
-        {
-            title: 'Rio Ice cream',
-            lat: 6.874687,
-            lng: 79.857813
-        },
-        {
-            title: 'Il Gelato - Odel',
-            lat: 6.919013,
-            lng: 79.865094
+// Class completely builds everything needed for each location marker.
+var MarkerInfo = function(title, lng, lat, wikiID, pageID, iconColor) {
+    var self = this;
+    this.title = title;
+    this.lng = lng;
+    this.lat = lat;
+    this.wikiID = wikiID;
+    this.pageID = pageID;
+    this.iconColor = iconColor;
+
+    this.getContent = function() {
+        var sourceURL = 'https://en.wikipedia.org/wiki/' + self.wikiID;
+        // variable for source link
+        var urls = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + self.wikiID;
+        // url variable for wikipedia api returning only extract, is fed into ajax call
+
+            $.ajax({
+                type: 'GET',
+                dataType: 'jsonp',
+                data: {},
+                url: urls
+            }).done(function(response) {
+                self.content.setContent('<div>' + self.title + '</div>');
+                document.getElementById('clicked-content').innerHTML = ('<div>' + response.query.pages[self.pageID].extract + '  <br>(Source: ' + '<a href=' + sourceURL + ' target="_blank">Wikipedia)</a>' + '</div>');
+                // sets infowindow content on marker click
+
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+                self.content.setContent('<div>' + 'Service Currently Unavailable (Try again later)' + '</div>');
+            });
+
+    };
+
+    this.infowindow = new google.maps.InfoWindow();
+
+    // This function takes in a COLOR, and then creates a new marker
+    // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+    // of 0, 0 and be anchored at 10, 34).
+    function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+            '|40|_|%E2%80%A2',
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34),
+            new google.maps.Size(21,34));
+        return markerImage;
+    }
+
+    // Style the markers a bit. This will be our listing marker icon.
+    var defaultIcon = makeMarkerIcon('0091ff');
+
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+
+    this.iconColor = function () {
+        // Two event listeners - one for mouseover, one for mouseout,
+        // to change the colors back and forth.
+        this.icon.addListener('mouseover', function () {
+            this.setIcon(highlightedIcon);
+        });
+
+        this.icon.addListener('mouseout', function () {
+            this.setIcon(defaultIcon);
+        });
+    };
+
+    this.marker = new google.maps.Marker({
+        position: new google.maps.LatLng(self.lng, self.lat),
+        map: map,
+        title: self.title,
+        icon: self.icon
+    });
+
+    // Opens the info window for the location marker.
+    this.openInfowindow = function() {
+        for (var i=0; i < viewModel.locations.length; i++) {
+            viewModel.locations[i].infowindow.close();
         }
-    ],
-    mapStyle: [
-        {
-            "stylers": [
-                {
-                    "saturation": -45
-                },
-                {
-                    "lightness": 13
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#8fa7b3"
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [
-                {
-                    "color": "#667780"
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#333333"
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                {
-                    "color": "#8fa7b3"
-                },
-                {
-                    "gamma": 2
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#a3becc"
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "geometry.stroke",
-            "stylers": [
-                {
-                    "color": "#7a8f99"
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#555555"
-                }
-            ]
-        },
-        {
-            "featureType": "road.local",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#a3becc"
-                }
-            ]
-        },
-        {
-            "featureType": "road.local",
-            "elementType": "geometry.stroke",
-            "stylers": [
-                {
-                    "color": "#7a8f99"
-                }
-            ]
-        },
-        {
-            "featureType": "road.local",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#555555"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#bbd9e9"
-                }
-            ]
-        },
-        {
-            "featureType": "administrative",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#525f66"
-                }
-            ]
-        },
-        {
-            "featureType": "transit",
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                {
-                    "color": "#bbd9e9"
-                },
-                {
-                    "gamma": 2
-                }
-            ]
-        },
-        {
-            "featureType": "transit.line",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#a3aeb5"
-                }
-            ]
-        }
-    ],
-    markerColor: [
-        {
-            defaultIcon: 'f2657a',  // Style the markers a bit. This will be our listing marker icon.
-            highlightedIcon: 'db6a36'  // Create a "highlighted location" when user mouses over the marker.
-        }
-    ]
+        map.panTo(self.marker.getPosition());
+        self.infowindow.setContent(self.content);
+        self.infowindow.open(map,self.marker);
+    };
+
+    // Assigns a click event listener to the marker to open the info window.
+    this.addListener = google.maps.event.addListener(self.marker,'click', (this.openInfowindow));
+
 };
 
 
 var viewModel = {
-
-    self:this,
-    map: {},
-    bounds: {},
-    infowindow: {},
-
-
-    locationsVM: ko.observableArray(), //Observeable Array for locations in Model Object
-    searchBoxVal: ko.observable(""),                  //Observable taken from Search Box value for search
-    markers: [],                                     // Create a new blank array for all the listing markers.
-
-
-
-    searchAddress: function(searchBoxVal) {
-        // Use the DOM search input as a value to pass it in the viewModel.searchBoxVal observable
-    },
-
-    updateMap: function() {
-    //    uses the input to search for location and gathers data from foursquare
-    },
-
-    //Updates Map and loads the titles from the array of hard coded locations when user initially opens the page
-    defaultLocations: function(title) {
-        Model.locations.title = title;
-    },
-
-    initMap: function() {
-
-        var mapDivID = document.getElementById('map');      //Create variable for Map on Div
-
-        map = new google.maps.Map(mapDivID,{       //Creates a new map - Location is Colombo, Sri Lanka.
-            center: {lat: 6.911652, lng: 79.849640},
-            zoom: 13,
-                styles: Model.mapStyle,
-                mapTypeControl: false
-        });
-        var geocoder = new google.maps.Geocoder();
-        var bounds = new google.maps.LatLngBounds();
-        var infowindow = new google.maps.InfoWindow();
-
-        // The following loop uses the lat and lng from the Model
-        // create an array of markers on initialize.
-        var locations = Model.locations;
-        for (var i = 0; i < locations.length; i++) {
-            // Get the position from the location array.
-            var position = new google.maps.LatLng(
-                locations[i].lat,
-                locations[i].lng
-            );
-            var title = locations[i].title;
-            // Create a marker per location, and put into markers array.
-            var mapMarker = new google.maps.Marker({
-                position: position,
-                title: title,
-                map: map,
-                animation: google.maps.Animation.DROP,
-                icon: Model.markerColor.defaultIcon,
-                id: i
-            });
-
-            // Push the marker to our array of markers.
-            viewModel.markers.push(mapMarker);
-
-            // Create an onclick event to open the large infowindow at each marker.
-            mapMarker.addListener('click', function() {
-                viewModel.populateInfoWindow(this, viewModel.infowindow);
-            });
-            // Two event listeners - one for mouseover, one for mouseout,
-            // to change the colors back and forth.
-            mapMarker.addListener('mouseover', function() {
-                this.setIcon(Model.markerColor.highlightedIcon);
-            });
-            mapMarker.addListener('mouseout', function() {
-                this.setIcon(Model.markerColor.defaultIcon);
-            });
-        }
-    },
-
-    // This function will loop through the markers array and display them all.
-    showListings: function showListings() {
-            // Extend the boundaries of the map for each marker and display the marker
-            for (var i = 0; i < viewModel.markers.length; i++) {
-                viewModel.markers[i].setMap(map);
-                viewModel.bounds.extend(viewModel.markers[i].position);
-            }
-            map.fitBounds(viewModel.bounds);
+    locations: [
+        {
+            title: 'Yala National Park',
+            lat: 6.426934,
+            lng: 81.308217,
+            wikiID: 'Yala_National_Park',
+            pageid: 560254
         },
-
-    // This function will loop through the listings and hide them all.
-    hideListings: function hideListings() {
-            for (var i = 0; i < viewModel.markers.length; i++) {
-                viewModel.markers[i].setMap(null);
-            }
+        {
+            title: 'Wilpattu National Park',
+            lat: 8.455214,
+            lng: 80.042180,
+            wikiID: 'Wilpattu_National_Park',
+            pageid: 15347986
         },
-
-
-    // This function populates the infowindow when the marker is clicked.
-    populateInfoWindow: function(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (viewModel.infowindow.marker != marker) {
-        viewModel.infowindow.marker = marker;
-        // Make sure the marker property is cleared if the infowindow is closed.
-        viewModel.infowindow.addListener('closeclick', function() {
-            viewModel.infowindow.marker = null;
-        });
-        infowindow.open(map, marker);
-            }
+        {
+            title: 'Gal Oya National Park',
+            lat: 7.228529,
+            lng: 81.471796,
+            wikiID: 'Gal_Oya_National_Park',
+            pageID: 23678456
+        },
+        {
+            title: 'Uda Walawe National Park',
+            lat: 6.473846,
+            lng: 80.898525,
+            wikiID: 'Udawalawe_National_Park',
+            pageID: 579524
+        },
+        {
+            title: 'Maduru Oya National Park',
+            lat: 7.525384,
+            lng: 81.256282,
+            wikiID: 'Maduru_Oya_National_Park',
+            padeID: 28595145
+        },
+        {
+            title: 'Horton Plains National Park',
+            lat: 6.802195,
+            lng: 80.806464,
+            wikiID: 'Horton_Plains_National_Park',
+            pageID: 509982
         }
-
-
+    ],
+    searchBox: ko.observable('')
 
 };
 
