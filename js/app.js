9368ff27dd9866ae1d35acf07625cbe0d70db3be
Christiaan Quyn <christianq010@gmail.com>
2017-02-27 09:59:21 +0530

    fix: map infowindows, markers and wiki ajax request succesfully fixed
    infowindows pull extract from wikipedia upon clicking, hover color functions over markers

--- js/app.js
+++ js/app.js
@@ -1,98 +1,13 @@
 
 // Class completely builds everything needed for each location marker.
-var MarkerInfo = function(title, lng, lat, wikiID, pageID, iconColor) {
+var MarkerInfo = function(title, lng, lat, wikiID, pageID, marker) {
     var self = this;
     this.title = title;
     this.lng = lng;
     this.lat = lat;
     this.wikiID = wikiID;
     this.pageID = pageID;
-    this.iconColor = iconColor;
-
-    this.getContent = function() {
-        var sourceURL = 'https://en.wikipedia.org/wiki/' + self.wikiID;
-        // variable for source link
-        var urls = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + self.wikiID;
-        // url variable for wikipedia api returning only extract, is fed into ajax call
-
-            $.ajax({
-                type: 'GET',
-                dataType: 'jsonp',
-                data: {},
-                url: urls
-            }).done(function(response) {
-                self.content.setContent('<div>' + self.title + '</div>');
-                document.getElementById('clicked-content').innerHTML = ('<div>' + response.query.pages[self.pageID].extract + '  <br>(Source: ' + '<a href=' + sourceURL + ' target="_blank">Wikipedia)</a>' + '</div>');
-                // sets infowindow content on marker click
-
-            }).fail(function(jqXHR, textStatus, errorThrown) {
-                console.log(jqXHR);
-                self.content.setContent('<div>' + 'Service Currently Unavailable (Try again later)' + '</div>');
-            });
-    };
-
-    this.infowindow = new google.maps.InfoWindow();
-
-
-    // This function takes in a COLOR, and then creates a new marker
-    // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-    // of 0, 0 and be anchored at 10, 34).
-    function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-            '|40|_|%E2%80%A2',
-            new google.maps.Size(21, 34),
-            new google.maps.Point(0, 0),
-            new google.maps.Point(10, 34),
-            new google.maps.Size(21,34));
-        return markerImage;
-    }
-
-    // Style the markers a bit. This will be our listing marker icon.
-    var defaultIcon = makeMarkerIcon('0091ff');
-
-    // Create a "highlighted location" marker color for when the user
-    // mouses over the marker.
-    var highlightedIcon = makeMarkerIcon('FFFF24');
-
-    this.iconColor = function () {
-        // Two event listeners - one for mouseover, one for mouseout,
-        // to change the colors back and forth.
-        this.icon.addListener('mouseover', function () {
-            this.setIcon(highlightedIcon);
-        });
-
-        this.icon.addListener('mouseout', function () {
-            this.setIcon(defaultIcon);
-        });
-    };
-
-    this.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(self.lng, self.lat),
-        map: map,
-        title: self.title,
-        icon: defaultIcon
-    });
-
-    this.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(self.lng, self.lat),
-        map: map,
-        title: self.title,
-        icon: self.icon
-    });
-
-    // Opens the info window for the location marker.
-    this.openInfowindow = function() {
-        for (var i=0; i < viewModel.locations.length; i++) {
-            viewModel.locations[i].infowindow.close();
-        }
-        map.panTo(self.marker.getPosition());
-        self.infowindow.setContent(self.content);
-        self.infowindow.open(map,self.marker);
-    };
-
-    // Assigns a click event listener to the marker to open the info window.
-    this.addListener = google.maps.event.addListener(self.marker,'click', (this.openInfowindow));
+    this.marker = marker;
 
 };
 
--- js/maps.js
+++ js/maps.js
@@ -1,4 +1,5 @@
 var map;
+var infowindow;
 
     function initMap() {
         //Scripts executed only after Google Maps API Loaded
@@ -158,10 +159,96 @@ var map;
                 mapTypeControl: false
             };
             map = new google.maps.Map(document.getElementById('map'), mapOptions);
-        } else {
-            //if no google object found, display error div
+            infowindow = new google.maps.InfoWindow();
+        }
+        else {
+            // if no google object found, display error div
             viewModel.mapUnavailable(true);
         }
+
+        for (var i = 0; i < viewModel.locations.length; i++) {
+            var self = viewModel.locations[i];
+            // This function takes in a COLOR, and then creates a new marker
+            // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+            // of 0, 0 and be anchored at 10, 34).
+            function makeMarkerIcon(markerColor) {
+                var markerImage = new google.maps.MarkerImage(
+                    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+                    '|40|_|%E2%80%A2',
+                    new google.maps.Size(21, 34),
+                    new google.maps.Point(0, 0),
+                    new google.maps.Point(10, 34),
+                    new google.maps.Size(21,34));
+                return markerImage;
+            }
+
+            // Style the markers a bit. This will be our listing marker icon.
+            var defaultIcon = makeMarkerIcon('0091ff');
+
+            // Create a "highlighted location" marker color for when the user
+            // mouses over the marker.
+            var highlightedIcon = makeMarkerIcon('FFFF24');
+
+            viewModel.locations[i].marker = new google.maps.Marker({
+                position: new google.maps.LatLng(self.lng, self.lat),
+                map: map,
+                title: self.title,
+                icon: defaultIcon,
+                wikiID: self.wikiID
+            });
+
+            // Two event listeners - one for mouseover, one for mouseout,
+            // to change the colors back and forth.
+            viewModel.locations[i].marker.addListener('mouseover', function () {
+                this.setIcon(highlightedIcon);
+            });
+
+            viewModel.locations[i].marker.addListener('mouseout', function () {
+                this.setIcon(defaultIcon);
+            });
+
+            // Opens the info window for the location marker.
+            function openInfowindow (marker) {
+
+                console.log(marker);
+
+                map.panTo(marker.getPosition());
+                infowindow.setContent(marker.title);
+                infowindow.open(map,marker);
+
+
+                var sourceURL = 'https://en.wikipedia.org/wiki/' + marker.wikiID;
+                // variable for source link
+                var urls = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + marker.wikiID;
+                // url variable for wikipedia api returning only extract, is fed into ajax call
+
+                $.ajax({
+                    type: 'GET',
+                    dataType: 'jsonp',
+                    data: {},
+                    url: urls
+                }).done(function(response) {
+                    console.log(marker);
+                    var extract = response.query.pages[Object.keys(response.query.pages)[0]].extract;
+
+                    infowindow.setContent(marker.title + '<br>' + extract);
+                    // self.content.setContent('<div>' + self.title + '</div>');
+                    // document.getElementById('clicked-content').innerHTML = ('<div>' + response.query.pages[self.pageID].extract + '  <br>(Source: ' + '<a href=' + sourceURL + ' target="_blank">Wikipedia)</a>' + '</div>');
+                    // // sets infowindow content on marker click
+
+                }).fail(function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR);
+                    // self.content.setContent('<div>' + 'Service Currently Unavailable (Try again later)' + '</div>');
+                });
+            }
+
+            // Assigns a click event listener to the marker to open the info window.
+            this.addListener = google.maps.event.addListener(self.marker,'click', function() {
+                openInfowindow(this);
+            });
+        }
+
+
     }
 
 
