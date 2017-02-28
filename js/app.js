aee15e844176bbe77af731c0ebe10e3291e13e2a
Christiaan Quyn <christianq010@gmail.com>
2017-02-28 16:57:52 +0530

    fix: toggle menu on nav sidebar, search li items styled

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // **** Menu | Navigation Bar Toggle Script ****** ////
-$("#menu-toggle").click(function(e) {
+$(".menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
