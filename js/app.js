868b454f898e21242dd580a31b0aa0c38e817ada
Christiaan Quyn <christianq010@gmail.com>
2017-03-03 09:56:43 +0530

    feat: toggled css class handled through knockoutJS viewmodel

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-// **** Menu | Navigation Bar Toggle Script ****** ////
-$(".menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
 
 // Main Class that serves as Blueprint for each Location Marker
 var MarkerInfo = function(title, lng, lat, wikiID, pageID, marker) {
@@ -27,6 +22,8 @@ var viewModel = {
         new MarkerInfo('Horton Plains National Park', 6.802195, 80.806464, 'Horton_Plains_National_Park', 509982),
         new MarkerInfo('Kaudulla National Park', 8.157000, 80.916800, 'Kaudulla_National_Park', 24761590)
     ],
+    //Observable for Menu | Navigation Bar Toggle Button
+    visibleMenu: ko.observable(false),
     //observable used for running a search against locations array
     searchBox: ko.observable(''),
     //observable to determine if error div should be shown
@@ -37,6 +34,12 @@ var viewModel = {
     }
 
 };
+
+//Toggle Button / Side Menu Function when Clicked
+    viewModel.clickMe = function() {
+        var self = this;
+        this.visibleMenu(!this.visibleMenu());
+    };
 
 // Search function for filtering through the list of locations based on the name of the location.
     viewModel.search = ko.dependentObservable(function() {
